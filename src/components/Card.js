e4c18488e012f188fa7591c39ba773b663315368
AlexV <alexartimon@gmail.com>
2021-08-06 13:32:15 +0300

    turned into CookingFriends

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
 
 	return (
-		<div className='tc bg-light-green dib br3 -pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robo' src={`https://robohash.org/${id}?200x200`} />
+		<div className='tc bg-grey dib br3 -pa3 ma2 grow bw2 shadow-5'>
+			<img alt='robo' src={`https://lorempixel.com/200/200/food/${id}`} />
 			<div>
 				<h2> {name} </h2>
 				<p>	{email} </p>
