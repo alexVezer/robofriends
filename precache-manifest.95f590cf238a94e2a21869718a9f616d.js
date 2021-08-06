a131c44b865558f4ce59011af6730fa73267d434
AlexV <alexartimon@gmail.com>
2021-08-06 14:36:11 +0300

    Updates

---
+++ precache-manifest.95f590cf238a94e2a21869718a9f616d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52de35c6a888ceb04fbf0c9d3b8d5dcc",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "33ea661074d8d8f77200",
+    "url": "/robofriends/static/css/2.25f7a212.chunk.css"
+  },
+  {
+    "revision": "0166d530991f85399c7a",
+    "url": "/robofriends/static/css/main.c92d3887.chunk.css"
+  },
+  {
+    "revision": "33ea661074d8d8f77200",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "0166d530991f85399c7a",
+    "url": "/robofriends/static/js/main.d5aaa4d5.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.1c33f54c07127dc147a95fbd17285ec3.js"
+  "/robofriends/precache-manifest.95f590cf238a94e2a21869718a9f616d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
